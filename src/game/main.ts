8a0a2e8cfe5cae033c5ca0bfc6637fcf18e491ee
Sanchedev <sanchedev@outlook.com>
2026-03-04 14:23:51 -0500

    feat: Add Peashooter plant with idle and shoot textures

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,23 +1,15 @@
 import { Game, Scene } from '../canvas-engine/index.js'
-import { loadTexture } from '../canvas-engine/load/textures.js'
-import { Sprite } from '../canvas-engine/nodes/sprite.js'
+import { Peashooter } from './plants/peashooter.js'
 
 const canvas = document.querySelector('canvas')!
 const ctx = canvas.getContext('2d')!
-
-await loadTexture('icon', 'assets/icon.png')
 
 Game.setup({
   canvas,
   ctx,
 })
 
-const mainScene = new Scene(
-  () =>
-    new Sprite({
-      textureId: 'icon',
-    }),
-)
+const mainScene = new Scene(() => Peashooter())
 
 Game.sceneManager.addScene('main', mainScene)
 Game.sceneManager.setScene('main')
---
+++ src/game/plants/peashooter.ts
@@ -0,0 +1,22 @@
+import { useNode } from '../../canvas-engine/lib/node.js'
+import { loadTexture } from '../../canvas-engine/load/textures.js'
+import { Sprite } from '../../canvas-engine/nodes/index.js'
+
+await loadTexture(
+  'peashooter.idle',
+  'assets/sprites/plants/peashooter/idle.png',
+)
+await loadTexture(
+  'peashooter.shoot',
+  'assets/sprites/plants/peashooter/shoot.png',
+)
+
+export function Peashooter() {
+  const { node, onStart } = useNode<Sprite>()
+
+  onStart((node) => {
+    console.log(node.id)
+  })
+
+  return node(new Sprite({ textureId: 'peashooter.idle' }))
+}
