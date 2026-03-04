7b67406848a512c8a8e176e7cbb646af5fb896f5
Sanchedev <sanchedev@outlook.com>
2026-03-04 13:25:50 -0500

    feat: Add icon asset and implement loading in main game script

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,10 +1,25 @@
-import { Game } from '../canvas-engine/index.js'
+import { Game, Scene } from '../canvas-engine/index.js'
+import { loadTexture } from '../canvas-engine/load/textures.js'
+import { Sprite } from '../canvas-engine/nodes/sprite.js'
 
 const canvas = document.querySelector('canvas')!
 const ctx = canvas.getContext('2d')!
+
+await loadTexture('icon', 'assets/icon.png')
 
 Game.setup({
   canvas,
   ctx,
 })
+
+const mainScene = new Scene(
+  () =>
+    new Sprite({
+      textureId: 'icon',
+    }),
+)
+
+Game.sceneManager.addScene('main', mainScene)
+Game.sceneManager.setScene('main')
+
 Game.play()
