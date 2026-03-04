6fefb39ee6b2dbc45bd20a8e8d0141ec3bb487ad
Sanchedev <sanchedev@outlook.com>
2026-03-04 16:47:38 -0500

    feat: Refactor Peashooter to include animation management and keyframe setup

--- src/game/plants/peashooter.ts
+++ src/game/plants/peashooter.ts
@@ -1,5 +1,14 @@
+import { Vector2 } from '../../canvas-engine/index.js'
 import { loadTexture } from '../../canvas-engine/load/textures.js'
-import { Sprite, useNode } from '../../canvas-engine/nodes/index.js'
+import {
+  kfFromProp,
+  multiKF,
+} from '../../canvas-engine/nodes/animation-player.js'
+import {
+  AnimationPlayer,
+  Sprite,
+  useNode,
+} from '../../canvas-engine/nodes/index.js'
 
 await loadTexture(
   'peashooter.idle',
@@ -14,8 +23,44 @@ export function Peashooter() {
   const { node, onStart } = useNode<Sprite>()
 
   onStart((node) => {
-    console.log(node.id)
+    const animPlayer = node.getChild<AnimationPlayer>('animation-player')
+
+    animPlayer
+      .add('idle', {
+        fps: 4,
+        keyframes: [
+          multiKF([
+            kfFromProp(node, 'textureId', 'peashooter.idle'),
+            kfFromProp(node, 'margin', new Vector2(0, 0)),
+          ]),
+          kfFromProp(node, 'margin', new Vector2(16, 0)),
+          kfFromProp(node, 'margin', new Vector2(32, 0)),
+          kfFromProp(node, 'margin', new Vector2(48, 0)),
+        ],
+        loop: false,
+      })
+      .add('shoot', {
+        fps: 4,
+        keyframes: [
+          multiKF([
+            kfFromProp(node, 'textureId', 'peashooter.shoot'),
+            kfFromProp(node, 'margin', new Vector2(0, 0)),
+          ]),
+          kfFromProp(node, 'margin', new Vector2(16, 0)),
+          kfFromProp(node, 'margin', new Vector2(32, 0)),
+          kfFromProp(node, 'margin', new Vector2(48, 0)),
+        ],
+        loop: false,
+      })
+
+    animPlayer.play('idle')
   })
 
-  return node(new Sprite({ textureId: 'peashooter.idle' }))
+  return node(
+    new Sprite({
+      textureId: 'peashooter.idle',
+      size: new Vector2(16, 16),
+      children: [new AnimationPlayer({})],
+    }),
+  )
 }
