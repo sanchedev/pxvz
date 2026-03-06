d1894e4d2d0951bf1a0dca8ddcd005e7d5a8ec7a
Sanchedev <sanchedev@outlook.com>
2026-03-06 11:27:26 -0500

    feat: Remove adds

--- packages/tiny-engine/src/hooks/adds/index.ts
+++
@@ -1,4 +0,0 @@
-export { useAdd } from './add.js'
-
-export { addStart } from './start.js'
-export { addUpdate } from './update.js'
--- packages/tiny-engine/src/hooks/index.ts
+++ packages/tiny-engine/src/hooks/index.ts
@@ -1,4 +1,3 @@
-export * from './adds/index.js'
 export * from './load.js'
 export * from './start.js'
 export * from './update.js'
