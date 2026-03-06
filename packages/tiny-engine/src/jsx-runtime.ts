449c6814e1ad1a10d191b5b25533b3bdcf735016
Sanchedev <sanchedev@outlook.com>
2026-03-06 13:11:54 -0500

    feat: Improve TinyComponent definition and set props managment in function jsx

--- packages/tiny-engine/src/jsx-runtime.ts
+++ packages/tiny-engine/src/jsx-runtime.ts
@@ -1,27 +1,36 @@
 import { finishHooks, startHooks } from './hooks/context.js'
+import {
+  type Elements,
+  Nodes,
+  type NodeToOptions,
+  type TypeElements,
+} from './nodes/types.js'
 import { Node, type NodeOptions } from './nodes/index.js'
 
-export type TinyComponent = typeof Node | ((props: any) => Node)
+export type TinyComponent =
+  | keyof JSX.IntrinsicElements
+  | typeof Node
+  | ((props: any) => Node)
 
-type PropsOf<T> = T extends (props: infer P) => Node
-  ? P
-  : T extends new (props: infer P) => Node
-    ? Omit<P, 'children'>
-    : never
+type PropsOf<T> = T extends keyof JSX.IntrinsicElements
+  ? JSX.IntrinsicElements[T]
+  : T extends (props: infer P) => Node
+    ? P
+    : T extends new (props: infer P) => Node
+      ? Omit<P, 'children'>
+      : never
 
-type ReturnTypeOf<T> = T extends (props: any) => infer R
-  ? R
-  : T extends new (props: any) => infer R
+type ReturnTypeOf<T> = T extends keyof JSX.IntrinsicElements
+  ? (typeof Nodes)[T]['prototype']
+  : T extends (props: any) => infer R
     ? R
-    : never
-
-type Children = Node | Node[]
-
-type WithChildren<T> = T & { children?: Children }
+    : T extends new (props: any) => infer R
+      ? R
+      : never
 
 export function jsx<T extends TinyComponent>(
   type: T,
-  props: WithChildren<PropsOf<T>>,
+  props: PropsOf<T>,
 ): ReturnTypeOf<T> {
   const { children } = props
 
@@ -31,7 +40,12 @@ export function jsx<T extends TinyComponent>(
 
   startHooks()
 
-  if (
+  if (typeof type === 'string') {
+    node = new (Nodes[type as keyof JSX.IntrinsicElements] as typeof Node)({
+      ...props,
+      children: safeChildren,
+    } as NodeToOptions<typeof Node>) as ReturnTypeOf<T>
+  } else if (
     typeof type === 'function' &&
     ('nodeName' in type ||
       type.prototype instanceof Node ||
@@ -57,10 +71,11 @@ export function jsx<T extends TinyComponent>(
 export const jsxs = jsx
 export const Fragment = (props: NodeOptions) => jsx(Node, props)
 
-type TinyElement<T extends Node = Node> = T
-
 declare global {
   namespace JSX {
-    type Element = TinyElement
+    type Element = Node
+
+    interface IntrinsicElements extends Elements {}
+    interface NodeTypes extends TypeElements {}
   }
 }
