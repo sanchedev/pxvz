d88f65f6dd37f0283261283b721eaa3d1ee5d693
Sanchedev <sanchedev@outlook.com>
2026-03-11 17:24:36 -0500

    refactor: Replace keyof typeof Nodes to NodeName and use NodeInstances

--- packages/tiny-engine/src/nodes/utils.ts
+++ packages/tiny-engine/src/nodes/utils.ts
@@ -1,17 +1,13 @@
 import { InvalidNodeInstanceError } from '../errors/node.js'
 import type { Node } from './node.js'
 import { Nodes } from './registry.js'
-import type { NodeClasses } from './types.js'
+import type { NodeInstances, NodeName } from './types.js'
 
 const nodeNamesMap = new Map<Node, keyof typeof Nodes>()
 
 // TODO: Arregla esto añadiendo un map para cache y usar object get property of
-export function getNodeName<T extends keyof NodeClasses>(
-  node: NodeClasses[T]['prototype'],
-): T {
-  const nodePrototype = Object.getPrototypeOf(
-    node,
-  ) as NodeClasses[T]['prototype']
+export function getNodeName<T extends NodeName>(node: NodeInstances[T]): T {
+  const nodePrototype = Object.getPrototypeOf(node) as NodeInstances[T]
   const name = nodeNamesMap.get(nodePrototype)
   if (name != null) return name as T
 
